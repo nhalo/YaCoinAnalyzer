1088353aa36f25dbd2a35634675b3366388b0987
nhalo <git.spamm@t-online.de>
2013-05-28 23:03:19 +0200

    [Added] Avg daily income in bar chart and new total transactions plot
    [*] Now the average daily income is drawn into to the daily income bar
    diagram as a horizontal line
    [*] There is a new total transactions plot, but no dates are displayed
    right now
    [*] Minor bugfixes to clear figure data after plotting

--- PlotterModule.py
+++ PlotterModule.py
@@ -27,9 +27,11 @@ def plotBarGraph(yacStruct, plotConfig):
     bins = []
 
     days = []
-    amountOfCoinsPerIntervallTotal = []
-    amountOfCoinsPerIntervallConfirmed = []
-    amountOfCoinsPerIntervallUnconfirmed = []
+    amountOfCoinsPerIntervallTotal          = []
+    amountOfCoinsPerIntervallConfirmed      = []
+    amountOfCoinsPerIntervallUnconfirmed    = []
+
+    averageDailyIncome                      = yacStruct.avgIncomePerDay 
 
     for day in yacStruct.daily:
         for key, value in day.items():
@@ -40,10 +42,10 @@ def plotBarGraph(yacStruct, plotConfig):
 
     #Make np-aray of days
     tmpDays = []
+    tmpInc  = []
     for index, day in enumerate(days):
         tmpDays.append(index)
-
-
+        tmpInc.append(averageDailyIncome)
 
     d = np.array(tmpDays)
 
@@ -52,12 +54,22 @@ def plotBarGraph(yacStruct, plotConfig):
     c_c = np.array(amountOfCoinsPerIntervallConfirmed)
     c_u = np.array(amountOfCoinsPerIntervallUnconfirmed)
 
+    a_i = np.array(tmpInc)
+
     #Get figure
     fig = plt.figure(1)
     
     #Plot bar diagramm
     p1 = plt.bar( d, c_u, color="red",alpha = 0.75, label="unconfirmed", align='center')
     p2 = plt.bar( d, c_c, color="blue",alpha = 0.75, label="confirmed", align='center', bottom=c_u)
+
+    #Plot average daily income
+    p3 = plt.plot( d, a_i,'k--', alpha = 0.75, label="daily avg")
+
+    #Rotate x-axis
+    fig.autofmt_xdate()
+
+
     plt.xlabel("Days")
     plt.ylabel("Mined YaCoins")
     plt.xticks(tmpDays, days)
@@ -78,5 +90,55 @@ def plotBarGraph(yacStruct, plotConfig):
     if showGraphs:
         plt.show()
 
+    fig.clear()
+
+def plotHistogramm():
+    pass
+
+def plotTransActionsOverTime():
+    pass
+
+def plotTransactionsTotal(yacStruct, plotConfig):
+    showGraphs                  = plotConfig.show
+    saveGraphs                  = plotConfig.save
+    plotPath                    = plotConfig.path
+    plotModes                   = plotConfig.modes
+     
+    nrOfTransactions            = 0
+    amountOfSingleTransactions   = []
+
+    for transaction in yacStruct.data:
+        amountOfSingleTransactions.append(transaction['Amount'])
+
+    nrOfTransactions = amountOfSingleTransactions.__len__()
+
+    n_t = np.arange(1, nrOfTransactions+1)
+    a_t = np.array(amountOfSingleTransactions)
+
+    #Get figure
+    fig = plt.figure(1)
+    
+    #Plot curve diagramm
+    p1 = plt.plot( n_t, a_t,'b-', alpha = 0.75)
+    plt.xlabel("Number of Transactions")
+    plt.ylabel("Mined YaCoins")
+
+    if saveGraphs:
+        pdf_page = PdfPages(plotPath+"YactransactionTotal.pdf")
+        pdf_page.savefig(bbox_inches='tight')
+        pdf_page.close()
+        
+        fig.savefig(plotPath+"YactransactionTotal.eps", format="eps")
+        fig.savefig(plotPath+"YactransactionTotal.svg", transparent=True, bbox_inches='tight')
+
+        print("Diagram written to disc!")
+
+    if showGraphs:
+        plt.show()
+
+    fig.clear()
+
+    
+    
     #histogram
     #cdf
